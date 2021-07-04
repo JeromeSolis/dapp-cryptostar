c20196ef0815a523fbdea3dc6cf2354394fc88fb
Jerome Solis <jay@Js-MacBook-Pro.local>
2021-07-03 20:27:42 -0400

    Tried unsuccessfully many truffle-config.js configuration for Rinkeby deployment

--- truffle-config.js
+++ truffle-config.js
@@ -18,7 +18,7 @@
  *
  */
 
-const HDWalletProvider = require('./app/node_modules/truffle-hdwallet-provider');
+const HDWalletProvider = require("./app/node_modules/@truffle/hdwallet-provider");
 const fs = require('fs');
 
 const infuraKey = fs.readFileSync(".key").toString().trim();
@@ -74,14 +74,16 @@ module.exports = {
     // },
 
     rinkeby: {
-      provider: function() {return new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`)},
+      provider: function() {
+        return new HDWalletProvider(mnemonic, `wss://rinkeby.infura.io/ws/v3/${infuraKey}`)
+      },
       network_id: 4,       // Rinkeby's id
-      gas: 4500000,
-      gasPrice: 10000000000,// Ropsten has a lower block limit than mainnet
-      // networkCheckTimeout: 10000000
+      networkCheckTimeout: 10000,
+      gas: 2500000,
+      gasPrice: 200000000000,
       // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
 
     // Useful for private networks
@@ -94,9 +96,22 @@ module.exports = {
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
+    // enableTimeouts: false
     // timeout: 100000
   },
 
   // Configure your compilers
-   
+  compilers: {
+    solc: {
+      version: "0.5.5",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  }
 }
